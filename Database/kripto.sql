b76b01e52326833a18b079dabd5a65507375b196
zulfi074 <90306400+zulfi074@users.noreply.github.com>
2023-11-23 07:40:06 +0700

    Add files via upload

---
+++ Database/kripto.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2023 at 08:36 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kripto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aduan`
+--
+
+CREATE TABLE `aduan` (
+  `id_aduan` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `nama` varchar(400) NOT NULL,
+  `kategori` varchar(400) NOT NULL,
+  `tanggal` varchar(400) NOT NULL,
+  `rincian` varchar(9999) NOT NULL,
+  `foto` varchar(400) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `aduan`
+--
+
+INSERT INTO `aduan` (`id_aduan`, `id`, `nama`, `kategori`, `tanggal`, `rincian`, `foto`) VALUES
+(49, 12, 'Jalan berlubang', 'Fasilitas', '2023-11-16', '9fMN8RWsDKqUs4DlAhrJ3myra23jTbb0wWhbUeHsZkFkGcp62rEFpW5JwuU+86w5', 'NW9za3VDV3o4RlZFb1VJRDZGT2UwWThEckNrSVNqSDU0cHVtT2pibmZRZz06OgUBOakprSIrtdXB0yDlJ9o=');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `nama`, `number`) VALUES
+(12, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin', '0'),
+(13, 'markhof44@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'gombong', '081456698900');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aduan`
+--
+ALTER TABLE `aduan`
+  ADD PRIMARY KEY (`id_aduan`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `aduan`
+--
+ALTER TABLE `aduan`
+  MODIFY `id_aduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
